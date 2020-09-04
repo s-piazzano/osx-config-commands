c1aafa5a3d02d3c3d2a518459866d272c7e039d2
Stefano Piazzano <piazzano.stefano@gmail.com>
2020-09-04 15:59:54 +0200

    initial commit

---
+++ osx-commands-setup.sh
@@ -0,0 +1,26 @@
+xcode-select --install
+#Instal Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+#Package
+    #Utils
+    brew install htop
+    brew install wget
+    brew install git
+    #Productivity
+    brew install rectangle
+#Install & configure NVM
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+    #Commands
+    nvm install (node version stable + latest)
+    nvm use (node version that you want use)
+#npm
+npm install eslint --save-dev
+#Browser
+brew cask install firefox
+#Code Editor
+brew cask install visual-studio-code
+    #extensions
+        #Bracket Pair Colorizer 2 
+        #DotENV
+        #Material Icon Theme
+        #Vetur
